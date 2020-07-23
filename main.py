29daf9c43b1254cea5b811fa3ce5e41cc3d5b4e8
Satoshi Watanabe <sassy.watanabe@gmail.com>
2020-07-23 10:49:09 +0900

    update ameblo script.

--- main.py
+++ main.py
@@ -20,23 +20,23 @@ def saveContent(url):
     filename = url.split("/")[-1]
     links.append(filename)
     f = open("contents/" + filename, 'w')
+    print(url)
     soup = BeautifulSoup(urlopen(url).read().decode('utf-8'), "html.parser")
-    f.write(soup.find("div", attrs={"class": "skin-entryInner"}).prettify())
+    f.write(soup.find("article").prettify())
     f.close()
 
-def parseContent(url):
+def parseContent(baseUrl, url):
     f = urlopen(url)
     soup = BeautifulSoup(f.read().decode('utf-8'), "html.parser")
-    titles = soup.find_all("h2", attrs={"amb-component": "entryItemTitle"})
+    titles = soup.find("ul", attrs={"class": "contentsList"}).find_all("li")
     for title in titles:
         link = title.find("a").get("href")
         schema = Schema(Url())
-        print(schema(link))
-        saveContent(schema(link))
+        saveContent(schema(baseUrl + link))
 
-    next = soup.find("a", attrs={"amb-component": "paginationNext"})
+    next = soup.find("a", attrs={"class": "pagingNext"})
     if next is not None:
-        return next.get("href")
+        return baseUrl + next.get("href")
     else:
         return None
 
@@ -45,12 +45,13 @@ if __name__ == '__main__':
         print("input ameblo id")
         sys.exit()
     amebloid = sys.argv[1]
-    url = "http://ameblo.jp/" + amebloid + "/entrylist.html"
+    baseUrl = "http://ameblo.jp/" 
+    url = baseUrl + amebloid + "/entrylist.html"
     try:
         os.mkdir("contents")
     except FileExistsError:
         pass
     while url is not None:
-        url = parseContent(url)
+        url = parseContent(baseUrl, url)
         time.sleep(3)
     createIndexPage()
